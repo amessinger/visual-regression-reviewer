653f35c8d2c26578c9b7f8dd4d5048dfdaa6b041
Antonin Messinger <antonin.messinger@gmail.com>
2017-01-20 16:51:56 +0100

    caches compiled templates

--- renderer.js
+++ renderer.js
@@ -5,8 +5,17 @@ const Handebars = require('handlebars');
 const rootDirectory = './templates';
 const extension = '.hbs';
 
+const compiledTemplatesCache = {};
+
+
+function getTemplate(templateName) {
+  if (!compiledTemplatesCache[templateName]) {
+    compiledTemplatesCache[templateName] = Handebars.compile(fs.readFileSync(path.join(rootDirectory, `${templateName}${extension}`), 'utf8'));
+  }
+  return compiledTemplatesCache[templateName];
+}
 
 exports.render = (templateName, data)=> {
-  let template = Handebars.compile(fs.readFileSync(path.join(rootDirectory, `${templateName}${extension}`), 'utf8'));
+  let template = getTemplate(templateName);
   return template(data);
 };
